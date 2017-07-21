258fad5af768a23e991fc2a9094a4d573844d414
sonworks <csw0413@outlook.com>
2017-07-21 10:42:12 +0900

    connected AFragment from MainActivity

--- app/src/main/java/com/example/sonworks/fragmentactivitysample/MainActivity.java
+++ app/src/main/java/com/example/sonworks/fragmentactivitysample/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.sonworks.fragmentactivitysample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.sonworks.fragmentactivitysample.fragment.AFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // TODO: Rename and change types of parameters
+        final String PARAM1 = "param1";
+        final String PARAM2 = "param2";
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().add(R.id.fragmentLayoutId, AFragment.newInstance(PARAM1, PARAM2)).commit();
+
     }
 }
